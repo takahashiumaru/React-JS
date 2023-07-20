f9b5d240a21824a4ada94a43a64cac5f290403dd
Umar Ma'ruf Mutaqin <umarmarufmutaqin@gmail.com>
2023-07-20 16:39:43 +0700

    ok

--- src/Root.js
+++ src/Root.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+import { Analytics } from '@vercel/analytics/react';
 const Root = () => {
   return (
     <Router basename="/"> {/* Replace "your-app-name" with your actual app name */}
       <App />
+      <Analytics />
     </Router>
   );
 };
